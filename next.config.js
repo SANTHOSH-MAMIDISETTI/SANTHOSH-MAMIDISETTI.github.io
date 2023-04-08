33d6fb7fe6456c7f13424bab4904df92c858de49
MAMIDISETTI SANTHOSH <mamidisettisanthosh2004@gmail.com>
2023-04-09 04:05:08 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,35 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/SANTHOSH-MAMIDISETTI",
-        permanent: true,
-      },
-      {
-        source: "/linkedin",
-        destination: "https://www.linkedin.com/in/santhosh-mamidisetti/",
-        permanent: true,
-      },
-      {
-        source: "/youtube",
-        destination: "https://www.youtube.com/@santhoshmamidisetti",
-        permanent: true,
-      },
-      {
-        source: "/instagram",
-        destination: "https://instagram.com/santhosh_mamidisetti",
-        permanent: true
-      },
-      {
-        source: "/twitter",
-        destination: "https://twitter.com/_m_santhosh_",
-        permanent: true
-      }
-    ];
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   async redirects() {
+//     return [
+//       {
+//         source: "/github",
+//         destination: "https://github.com/SANTHOSH-MAMIDISETTI",
+//         permanent: true,
+//       },
+//       {
+//         source: "/linkedin",
+//         destination: "https://www.linkedin.com/in/santhosh-mamidisetti/",
+//         permanent: true,
+//       },
+//       {
+//         source: "/youtube",
+//         destination: "https://www.youtube.com/@santhoshmamidisetti",
+//         permanent: true,
+//       },
+//       {
+//         source: "/instagram",
+//         destination: "https://instagram.com/santhosh_mamidisetti",
+//         permanent: true
+//       },
+//       {
+//         source: "/twitter",
+//         destination: "https://twitter.com/_m_santhosh_",
+//         permanent: true
+//       }
+//     ];
+//   },
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig
